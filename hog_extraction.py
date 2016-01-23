c3b6c0744df31c5bb9770366cb0b946595f6fc41
Baptiste Bauvin <baptiste.bauvin@centrale-marseille.fr>
2016-01-24 00:48:42 +0100

    Seems functional, need to test it on all DB

--- hog_extraction.py
+++ hog_extraction.py
@@ -50,9 +50,7 @@ def reSize(image, CELL_DIMENSION):
   return resizedImage
 
 def hogAllBlocks(blocks):
-  print blocks[0][0].shape
   gradients = np.array([np.array([hog(cv2.cvtColor(block, cv2.COLOR_BGR2GRAY), orientations=8, pixels_per_cell=(5,5), cells_per_block=(1,1)) for block in image]) for image in blocks])
-  print gradients.shape
   return gradients
 
 def clusterGradients(gradients, NB_CLUSTERS, MAXITER):
@@ -112,77 +110,3 @@ if __name__ == '__main__':
   print "Computed histograms in " + str(int(end - hogedTime)) + 'sec'
   print "Total time : " + str(end-start) + 'sec'
   print "Histogram shape : " +str(histograms.shape)
-
-
-
-# def even(difference):
-#   return not(difference % 2)
-
-# def findmaxDim(npImages):
-  
-#   max_height = 0
-#   max_width = 0
-  
-#   for npImage in npImages:
-#     height, width, channels = cv2.imread(npImage[1]).shape
-#     if height > max_height:
-#       max_height=height
-#     if width > max_width:
-#       max_width=width
-#   return [max_height, max_width]
-
-# def resizeImage(image, height, width):
-#   ratio = float(8000000)/(width*height)
-#   smallImage = cv2.resize(image, (0,0), fx=ratio, fy=ratio)
-#   return smallImage 
-
-# def enlarge(image, maxDimension, color):
-
-#   height, width, channels = image.shape
-#   [height_difference, width_difference] = np.array(maxDimension) - np.array([height, width])
-#   print(height_difference, width_difference)
-#   if even(height_difference) and even(width_difference):
-#     treatedImage = cv2.copyMakeBorder(image, height_difference/2, height_difference/2, width_difference/2, width_difference/2, cv2.BORDER_CONSTANT, value=color)
-    
-#   elif even(height_difference):
-#     treatedImage = cv2.copyMakeBorder(image, height_difference/2, height_difference/2, width_difference/2+1, width_difference/2, cv2.BORDER_CONSTANT, value=color)
-    
-#   elif even(width_difference):
-#     treatedImage = cv2.copyMakeBorder(image, height_difference/2+1, height_difference/2, width_difference/2, width_difference/2, cv2.BORDER_CONSTANT, value=color)
-    
-#   else:
-#     treatedImage = cv2.copyMakeBorder(image, height_difference/2+1, height_difference/2, width_difference/2+1, width_difference/2, cv2.BORDER_CONSTANT, value=color)
-    
-#   return treatedImage
-
-# def calcHog(npImages, color, maxDimension):
-
-#   list_hog = []
-#   hog = cv2.HOGDescriptor()
-
-#   # poulet = preTreat(cv2.imread(npImages[0][1]), maxDimension, color)
-#   for npImage in npImages:
-#     image = cv2.imread(npImage[1])
-#     height, width, channels = image.shape
-
-#     if height * width > 8000000:
-#       g = hog.compute(resizeImage(image, height, width))
-#     else:
-#       g = hog.compute(enlarge(image, maxDimension, color))
-#     print g.shape
-#   # list_hog = [hog.compute(cv2.imread(npImage[1])) for npImage in npImages]
-#   return list_hog
-
-# color=[0,0,0]
-# path ='../../03-jeux-de-donnees/101_ObjectCategories'
-
-# # get dictionary to link classLabels Text to Integers
-# sClassLabels = getClassLabels(path)
-
-# # Get all path from all images inclusive classLabel as Integer
-# dfImages = imgCrawl(path, sClassLabels)
-
-# npImages = dfImages.values
-# maxDimension = findmaxDim(npImages)
-# list_hog = calcHog(npImages, color, maxDimension)
-# print len(list_hog)
