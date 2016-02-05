887484487324acb479508c8be922e72412cc06f9
nikolasph <nikolas.huelsmann@gmail.com>
2016-02-05 09:28:20 +0100

    Bugfixxes + Time

--- Raw Code/FeatExtraction/ExecFeatExtraction.py
+++ Raw Code/FeatExtraction/ExecFeatExtraction.py
@@ -8,6 +8,7 @@ import cv                       # for OpenCV
 import datetime                 # for TimeStamp in CSVFile
 from scipy.cluster.vq import *  # for Clustering http://docs.scipy.org/doc/scipy/reference/cluster.vq.html
 import numpy as np              # for arrays
+import time                     # for time calculations
 
 # Import 3rd party modules
 
@@ -32,6 +33,8 @@ nameDB = "CT-Mini"
 
 print "Start:\t Exportation of images from DB"
 
+t_db_start = time.time()
+
 # get dictionary to link classLabels Text to Integers
 sClassLabels = DBCrawl.getClassLabels(path)
 
@@ -40,22 +43,28 @@ dfImages,nameDB = DBCrawl.imgCrawl(path, sClassLabels, nameDB)
 
 print "Done:\t Exportation of images from DB"
 
+t_db  = t_db_start - time.time()
 
 ################################ Feature Extraction
 print "Start:\t Features Extraction"
 
 ### Setup RGB
+t_rgb_start = time.time()
 print "RGB:\t Start"
+
 numberOfBins = 16
 maxColorIntensity = 256
 boolNormMinMax = False
  
 # Extract Feature from DB
 rgb_feat_desc,rgb_f_extr_res = FeatExtraction.calcRGBColorHisto(nameDB, dfImages, numberOfBins, maxColorIntensity, boolNormMinMax)
-print "RGB:\t Done"
+
+t_rgb = time.time() - t_rgb_start
+print "RGB:\t Done in: " + str(t_rgb) + "[s]"
 
 
 ### Setup HSV
+t_hsv_start = time.time()
 print "HSV:\t Start"
 h_bins = 8 
 s_bins = 3
@@ -65,10 +74,13 @@ boolNormMinMax = False
 
 # Extract Feature from DB
 hsv_feat_desc,hsv_f_extr_res = FeatExtraction.calcHSVColorHisto(nameDB, dfImages, histSize, boolNormMinMax)
-print "HSV:\t Done"
+t_hsv = time.time() - t_hsv_start
+print "HSV:\t Done in: " + str(t_hsv) + "[s]"
+
 
 
 ### Setup SIFT
+t_sift_start = time.time()
 print "SIFT:\t Start"
 boolSIFT = True
 cluster = 50
@@ -76,10 +88,12 @@ boolNormMinMax = False
 
 sift_descriptors,sift_des_list = FeatExtraction.calcSURFSIFTDescriptors(dfImages, boolSIFT)
 sift_feat_desc,sift_f_extr_res = FeatExtraction.calcSURFSIFTHisto(nameDB, dfImages, cluster, boolNormMinMax, sift_descriptors, sift_des_list, boolSIFT)
-print "SIFT:\t Done"
+t_sift = time.time() - t_sift_start 
+print "SIFT:\t Done in: " + str(t_sift) + "[s]"
 
 
 ### Setup SURF
+t_surf_start = time.time()
 print "SURF:\t Start"
 boolSIFT = False
 cluster = 50
@@ -88,20 +102,23 @@ boolNormMinMax = False
 # Extract Feature from DB
 surf_descriptors,surf_des_list = FeatExtraction.calcSURFSIFTDescriptors(dfImages, boolSIFT)
 surf_feat_desc,surf_f_extr_res = FeatExtraction.calcSURFSIFTHisto(nameDB, dfImages, cluster, boolNormMinMax, surf_descriptors, surf_des_list, boolSIFT)
-print "SURF:\t Done"
-
+t_surf = time.time() - t_sift_start 
+print "SURF:\t Done in: " + str(t_surf) + "[s]"
 
 ### Setup HOG
-#print "HOG:\t Start"
-#CELL_DIMENSION = 5
-#NB_ORIENTATIONS = 8
-#NB_CLUSTERS = 12
-#MAXITER = 100
+t_hog_start = time.time()
+print "HOG:\t Start"
+CELL_DIMENSION = 5
+NB_ORIENTATIONS = 8
+NB_CLUSTERS = 12
+MAXITER = 100
+NB_CORES = 1
 
 # Extract Feature from DB
-#hog_feat_desc,hof_f_extr_res = FeatExtraction.calcHOGParallel(nameDB, npImages, CELL_DIMENSION, NB_ORIENTATIONS, NB_CLUSTERS, MAXITER, NB_CORES)
-#hog_feat_desc,hog_f_extr_res = FeatExtraction.calcHOG(nameDB, dfImages, CELL_DIMENSION, NB_ORIENTATIONS, NB_CLUSTERS, MAXITER)   
-#print "HOG:\t Done"
+hog_feat_desc,hof_f_extr_res = FeatExtraction.calcHOGParallel(nameDB, dfImages.values, CELL_DIMENSION, NB_ORIENTATIONS, NB_CLUSTERS, MAXITER, NB_CORES)
+#hog_feat_desc,hog_f_extr_res = FeatExtraction.calcHOG(nameDB, dfImages.values, CELL_DIMENSION, NB_ORIENTATIONS, NB_CLUSTERS, MAXITER)   
+t_hog = time.time() - t_sift_start
+print "SURF:\t Done in: " + str(t_hog) + "[s]"
 
 print "Done:\t Features Extraction"
 
@@ -133,7 +150,7 @@ fileName = datetime.datetime.now().strftime("%Y_%m_%d") + "-" + surf_feat_desc
 ExportResults.exportNumpyToCSV(surf_f_extr_res, fileName)
 
 ### HOG
-#fileName = datetime.datetime.now().strftime("%Y_%m_%d") + "-" + hog_feat_desc
-#ExportResults.exportNumpyToCSV(hog_f_extr_res, fileName)
+fileName = datetime.datetime.now().strftime("%Y_%m_%d") + "-" + hog_feat_desc
+ExportResults.exportNumpyToCSV(hog_f_extr_res, fileName)
 
 print "Done:\t Save Features to CSV Databases"
