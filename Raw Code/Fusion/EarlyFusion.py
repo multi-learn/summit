78cb73e9928a6593bf7c6bb0018cb0f2d6fe22af
Baptiste Bauvin <baptiste.bauvin@centrale-marseille.fr>
2016-02-07 10:24:55 +0100

    Starting LateFusion

--- Raw Code/Fusion/EarlyFusion.py
+++ Raw Code/Fusion/EarlyFusion.py
@@ -7,14 +7,19 @@ import numpy as np
 # Linear Weighted Fusion
 # Bayesian Inference  /!\ Statistically independant => ? 
 # Dempster Schafer Theory  /!\ Inunderstandable
-# Dynamic Bayesian Method
+# Dynamic Bayesian Networks
 # Neural Network
 # Maximum Entropy Model
 # Kalman Filter
 # Particle Filter
 
 def linearWeightedFusion (toFuse, weights):
+	
+	# Normalize weights ?
+	# weights = weights/float(max(weights))
+
 	fused = np.array([component*weights for (component, weight) in zip(toFuse, weights)])
+	
 	return fused
 
 
