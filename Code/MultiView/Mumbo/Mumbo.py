b7797ab931bf473d406c1578f94aa9f8513708fd
bbauvin <baptiste.bauvin@centrale-marseille.fr>
2016-04-01 15:44:39 +0200

    Mumbo

--- Code/MultiView/Mumbo/Mumbo.py
+++ Code/MultiView/Mumbo/Mumbo.py
@@ -1,13 +1,109 @@
 import numpy as np
+import math
 
 def initialize(NB_CLASS, NB_VIEW, NB_ITER, DATASET_LENGTH, CLASS_LABELS):
     costMatrices = np.array([
                     np.array([
                         np.array([
-                            np.array([ 1 if CLASS_LABELS[exampleIndice]!=classe
+                            np.array([1 if CLASS_LABELS[exampleIndice]!=classe
                                        else -(NB_CLASS-1)
                                        for classe in range(NB_CLASS)
                                     ]) for exampleIndice in range(DATASET_LENGTH)
-                            ]) for viewIndice in range(NB_VIEW)]) for iteration in range(NB_ITER)
+                            ]) for viewIndice in range(NB_VIEW)]) for iteration in range(NB_ITER+1)
                     ])
-    return costMatrices, generalCostMatrix, f
+    generalCostMatrix = np.array([
+                            np.array([
+                                np.array([1 if CLASS_LABELS[exampleIndice]!=classe
+                                           else -(NB_CLASS-1)
+                                           for classe in range(NB_CLASS)
+                                ]) for exampleIndice in range(DATASET_LENGTH)
+                            ]) for iteration in range(NB_ITER)
+                        ])
+    fs = np.zeros((NB_ITER, NB_VIEW, DATASET_LENGTH, NB_CLASS))
+    ds = np.zeros((NB_ITER, NB_VIEW, DATASET_LENGTH))
+    edges = np.zeros((NB_ITER, NB_VIEW, DATASET_LENGTH))
+    alphas = np.zeros((NB_ITER, NB_VIEW))
+    predictions = np.zeros((NB_ITER, NB_VIEW, DATASET_LENGTH))
+    generalAlphas = np.zeros(NB_ITER)
+    generalFs = np.zeros((NB_ITER, DATASET_LENGTH, NB_CLASS))
+    return costMatrices, generalCostMatrix, fs, ds, edges, alphas, predictions, generalAlphas, generalFs
+
+
+def trainWeakClassifers(classifer, costMatrices):
+    return
+
+
+def computeEdge (predictionMatrix, costMatrix):
+    return np.sum(predictionMatrix*costMatrix)
+
+
+def computeAlpha(edge):
+    return 0.5*math.log((1+edge)/(1-edge))
+
+
+def allViewsClassifyWell(predictions, pastIterIndice, NB_VIEW, CLASS_LABEL, exampleIndice):
+    bool = True
+    for viewIndice in range(NB_VIEW):
+        if predictions[pastIterIndice, viewIndice, exampleIndice]!=CLASS_LABEL:
+            bool = False
+    return bool
+
+
+def updateDs(ds, predictions, CLASS_LABELS, NB_VIEW, DATASET_LENGTH, NB_CLASS, iterIndice):
+    for viewIndice in range(NB_VIEW):
+        for exampleIndice in range(DATASET_LENGTH):
+            for pastIterIndice in range(iterIndice):
+                if predictions[pastIterIndice, viewIndice, exampleIndice]==CLASS_LABELS[exampleIndice] or allViewsClassifyWell(predictions, pastIterIndice, NB_VIEW, CLASS_LABELS[exampleIndice], exampleIndice):
+                    ds[pastIterIndice, viewIndice, exampleIndice]=1
+                else:
+                    ds[pastIterIndice, viewIndice, exampleIndice]=0
+    return ds
+
+
+def updateFs(predictions, ds, alphas, fs, iterIndice, NB_VIEW, DATASET_LENGTH, NB_CLASS, CLASS_LABELS):
+    for viewIndice in range(NB_VIEW):
+        for exampleIndice in range(DATASET_LENGTH):
+            for classe in range(NB_CLASS):
+                fs[iterIndice, viewIndice, exampleIndice, classe] = np.sum(np.array([alphas[pastIterIndice, viewIndice]*ds[pastIterIndice, viewIndice, exampleIndice] if predictions[pastIterIndice, viewIndice, exampleIndice]==CLASS_LABELS[exampleIndice] else 0 for pastIterIndice in range(iterIndice)]))
+    return fs
+
+def updateCostmatrices(costMatrices, fs, iterIndice, NB_VIEW, DATASET_LENGTH, NB_CLASS, CLASS_LABELS):
+    for viewIndice in range(NB_VIEW):
+        for exampleIndice in range(DATASET_LENGTH):
+            for classe in range(NB_CLASS):
+                if classe != CLASS_LABELS[exampleIndice]:
+                    costMatrices[iterIndice, viewIndice, exampleIndice, classe] = math.exp(fs[iterIndice, viewIndice, exampleIndice, classe] - fs[iterIndice, viewIndice, exampleIndice, CLASS_LABELS[exampleIndice]])
+                else:
+                    costMatrices[iterIndice, viewIndice, exampleIndice, classe] = -1*np.sum(np.exp(fs[iterIndice, viewIndice, exampleIndice] - fs[iterIndice, viewIndice, exampleIndice, classe]))
+
+    return costMatrices
+
+
+def chooseView(predictions, generalCostMatrix, iterIndice, NB_VIEW):
+    edges = np.array([computeEdge(predictions[iterIndice, viewIndice], generalCostMatrix) for viewIndice in range(NB_VIEW)])
+    bestView = np.argmax(edges)
+    return bestView, edges[bestView]
+
+
+def updateGeneralFs(generalFs, iterIndice, predictions, alphas, DATASET_LENGTH, NB_CLASS, bestView, generalAlpha, CLASS_LABELS):
+    for exampleIndice in range(DATASET_LENGTH):
+        for classe in range(NB_CLASS):
+            generalFs[iterIndice, exampleIndice, classe]=np.sum(np.array([generalAlpha[pastIterIndice] if  predictions[pastIterIndice, bestView, exampleIndice]==CLASS_LABELS[exampleIndice] else 0 for pastIterIndice in range(iterIndice)]))
+        return generalFs
+
+
+def updateGeneralCostMatrix(generalCostMatrix, generalFs, iterIndice, DATASET_LENGTH, NB_CLASS, CLASS_LABELS):
+    for exampleIndice in range(DATASET_LENGTH):
+        for classe in range(NB_CLASS):
+            if classe != CLASS_LABELS[exampleIndice]:
+                generalCostMatrix[iterIndice, exampleIndice, classe]=math.exp(generalFs[iterIndice, exampleIndice, classe] - generalFs[iterIndice, exampleIndice, CLASS_LABELS[exampleIndice]])
+            else:
+                generalCostMatrix[iterIndice, exampleIndice, classe]= -1 * np.sum(np.exp(generalFs[iterIndice, exampleIndice] - generalFs[iterIndice, exampleIndice, classe]))
+    return generalCostMatrix
+
+def mumbo(DATASET, CLASS_LABELS, NB_CLASS, NB_VIEW, NB_ITER, DATASET_LENGTH):
+    costMatrices, generalCostMatrix, fs, ds, edges, alphas, predictions, generalAlphas, generalFs = initialize(NB_CLASS, NB_VIEW, NB_ITER, DATASET_LENGTH, CLASS_LABELS)
+    for iterIndice in range(NB_ITER):
+        poulet = trainWeakClassifers(classifer, costMatrices)
+        for viewIndice in range(NB_VIEW):
+            edges[iterIndice, viewIndice] = computeEdge(predictions[iterIndice, viewIndice], costMatrices[])
