ba3adb9b7d892c38ce706c1f351486ede7ac5725
bbauvin <baptiste.bauvin@centrale-marseille.fr>
2016-08-18 11:11:01 -0400

    OK for early fusion

--- Code/MultiView/Fusion/Methods/EarlyFusion.py
+++ Code/MultiView/Fusion/Methods/EarlyFusion.py
@@ -5,28 +5,49 @@ from MonoviewClassifiers import *
 
 import numpy as np
 
-def makemonoviewData(DATASET, weights):
-    NB_VIEW = len(DATASET)
-    DATASET_LENGTH = len(DATASET[0])
-    weightedMonoviewData = np.array([np.concatenate([weights[viewIndex]*DATASET[viewIndex][exampleIndex]
-                                                     for viewIndex in np.arange(NB_VIEW)])
-                                     for exampleIndex in np.arange(DATASET_LENGTH)])
-    return  weightedMonoviewData
 
-def linearWeightedTrain(DATASET, CLASS_LABELS, DATASET_LENGTH, NB_VIEW, monoviewClassifier,
-                   monoviewClassifierConfig, fusionConfig):
-    weights = map(float, fusionConfig)
-    weightedMonoviewData = makemonoviewData(DATASET, weights)
-    monoviewClassifierModule = globals()[monoviewClassifier]
-    classifier = monoviewClassifierModule.train(weightedMonoviewData, CLASS_LABELS, monoviewClassifierConfig)
-    return (classifier, weights)
+class EarlyFusionClassifier:
+    def __init__(self, monoviewClassifiersNames, monoviewClassifiersConfigs, NB_CORES=1):
+        self.monoviewClassifiersName = monoviewClassifiersNames[0]
+        self.monoviewClassifiersConfig = monoviewClassifiersConfigs[0]
+        self.monoviewClassifier = None
+        self.nbCores = NB_CORES
+        self.monoviewData = None
+
+    def makeMonoviewData(self, DATASET, weights=None):
+        NB_VIEW = DATASET.get("nbView").value
+        DATASET_LENGTH = DATASET.get("datasetLength").value
+        if weights==None:
+            weights = np.array([1/NB_VIEW for i in range(NB_VIEW)])
+        if sum(weights)!=1:
+            weights = weights/sum(weights)
+        self.monoviewData = np.concatenate([weights[viewIndex]*DATASET["/View"+str(viewIndex)+"/matrix"][...]
+                                                         for viewIndex in np.arange(NB_VIEW)], axis=1)
 
 
-def linearWeightedPredict(DATASET, fusionClassifier):
-    classifier, weights = fusionClassifier
-    weightedMonoviewData = makemonoviewData(DATASET, weights)
-    predictedLabels = classifier.predict(weightedMonoviewData)
-    return predictedLabels
+class WeightedLinear(EarlyFusionClassifier):
+    def __init__(self, NB_CORES=1, **kwargs):
+        EarlyFusionClassifier.__init__(self, kwargs['monoviewClassifiersNames'], kwargs['monoviewClassifiersConfigs'],
+                                      NB_CORES=NB_CORES)
+        self.weights = np.array(map(float, kwargs['fusionMethodConfig'][0]))
 
-def getConfig(fusionMethodConfig, monoviewClassifier, fusionClassifierConfig):
-    return
+    def fit_hdf5(self, DATASET, trainIndices=None):
+        if not trainIndices:
+            trainIndices = range(DATASET.get("datasetLength").value)
+        self.makeMonoviewData(DATASET, weights=self.weights)
+        monoviewClassifierModule = globals()[self.monoviewClassifiersName]
+        self.monoviewClassifier = monoviewClassifierModule.fit(self.monoviewData, DATASET["/Labels/labelsArray"][...],
+                                                               NB_CORES=self.nbCores,
+                                                               **dict((str(configIndex),config) for configIndex,config in
+                                                                      enumerate(self.monoviewClassifiersConfig)))
+
+    def predict_hdf5(self, DATASET, usedIndices=None):
+        if usedIndices == None:
+            usedIndices = range(DATASET.get("datasetLength").value)
+        if usedIndices:
+            self.makeMonoviewData(DATASET, weights=self.weights)
+            predictedLabels = self.monoviewClassifier.predict(self.monoviewData)
+        else:
+            predictedLabels=[]
+        return predictedLabels
+
--- Code/MultiView/run.py
+++ Code/MultiView/run.py
@@ -1,4 +1,4 @@
 import os
-os.system('python ExecMultiview.py -log --name MultiOmic --type .hdf5 --views Methyl:MiRNA:RNASEQ:Clinical --pathF /home/bbauvin/Documents/Data/Data_multi_omics/ --CL_split 0.3 --CL_nbFolds 4 --CL_nb_class 2 --CL_classes Positive:Negative --CL_type Fusion --CL_cores 4 --FU_method SVMForLinear')
+os.system('python ExecMultiview.py -log --name MultiOmic --type .hdf5 --views Methyl:MiRNA:RNASEQ:Clinical --pathF /home/bbauvin/Documents/Data/Data_multi_omics/ --CL_split 0.3 --CL_nbFolds 4 --CL_nb_class 2 --CL_classes Positive:Negative --CL_type Fusion --CL_cores 4 --FU_type EarlyFusion --FU_method WeightedLinear')
 # /donnees/pj_bdd_bbauvin/Data_multi_omics/
 # --MU_type DecisionTree:DecisionTree:DecisionTree:DecisionTree --MU_config 1:0.015 1:0.015 1:0.1 2:0.3 --MU_iter 100
