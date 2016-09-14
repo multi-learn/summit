b505cfc141665ec698cf17a2fecaa6e616392b40
bbauvin <baptiste.bauvin@centrale-marseille.fr>
2016-09-14 16:17:35 -0400

    Trying to fix segmentation fault

--- Code/MonoMutliViewClassifiers/Monoview/ExecClassifMonoView.py
+++ Code/MonoMutliViewClassifiers/Monoview/ExecClassifMonoView.py
@@ -56,9 +56,9 @@ def ExecMonoview(X, Y, name, learningRate, nbFolds, nbCores, databaseType, path,
     fileCL = kwargs["fileCL"]
     fileCLD = kwargs["fileCLD"]
     CL_type = kwargs["CL_type"]
-    datasetLength = X.shape[0]
     nbClass = kwargs["nbClass"]
     X = getValue(X)
+    datasetLength = X.shape[0]
     clKWARGS = kwargs[kwargs["CL_type"]+"KWARGS"]
 
     # Determine the Database to extract features
