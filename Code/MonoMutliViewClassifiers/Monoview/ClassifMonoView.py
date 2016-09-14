1f7bdff1dc1ca9a66f31af5c08c2fc97374a5f8a
bbauvin <baptiste.bauvin@centrale-marseille.fr>
2016-09-14 16:06:35 -0400

    Trying to fix segmentation fault

--- Code/MonoMutliViewClassifiers/Monoview/ClassifMonoView.py
+++ Code/MonoMutliViewClassifiers/Monoview/ClassifMonoView.py
@@ -22,15 +22,25 @@ __status__ 	= "Prototype"                           # Production, Development, P
 __date__	= 2016-03-25
 
 
+def isUseful (labelSupports, index, CLASS_LABELS, labelDict):
+    if labelSupports[labelDict[CLASS_LABELS[index]]] != 0:
+        labelSupports[labelDict[CLASS_LABELS[index]]] -= 1
+        return True, labelSupports
+    else:
+        return False, labelSupports
+
+
 def getLabelSupports(CLASS_LABELS):
     labels = set(CLASS_LABELS)
     supports = [CLASS_LABELS.tolist().count(label) for label in labels]
     return supports, dict((label, index) for label, index in zip(labels, range(len(labels))))
 
+
 def splitDataset(LABELS, NB_CLASS, LEARNING_RATE, DATASET_LENGTH):
     validationIndices = extractRandomTrainingSet(LABELS, 1-LEARNING_RATE, DATASET_LENGTH, NB_CLASS)
     validationIndices.sort()
     return validationIndices
+
 
 def extractRandomTrainingSet(CLASS_LABELS, LEARNING_RATE, DATASET_LENGTH, NB_CLASS):
     labelSupports, labelDict = getLabelSupports(np.array(CLASS_LABELS))
