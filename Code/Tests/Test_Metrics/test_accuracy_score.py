996cf32f7a3bc1b679fd8a485aa01ea121cff911
Baptiste Bauvin <baptiste.bauvin.1@ulaval.ca>
2017-11-14 12:28:57 -0500

    Added test Metrics

--- Code/Tests/Test_Metrics/test_accuracy_score.py
+++ Code/Tests/Test_Metrics/test_accuracy_score.py
@@ -3,19 +3,11 @@ import os
 
 from ...MonoMultiViewClassifiers.Metrics import accuracy_score
 
-# class Test_accuracy_score(unittest.TestCase):
-#
-#     def setUpClass(cls):
-#         pass
-#
-#
-#     def test_all_scores(cls):
-#         for filename in os.listdir("Code/MonoMultiviewClassifiers/Metrics/"):
-#             if filename != "__init__.py":
-#                 metric_name = filename[:-3]
-#                 metric_module = getattr(Metrics, metric_name)
-#                 cls.score_test(metric_module)
-#
-#
-#     def score_test(cls, metric_module):
-#         pass
+
+class Test_accuracy_score(unittest.TestCase):
+
+    def setUpClass(cls):
+        pass
+
+    def score_test(cls, metric_module):
+        pass
