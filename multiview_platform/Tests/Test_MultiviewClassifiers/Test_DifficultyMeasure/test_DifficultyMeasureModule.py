346cbaca71875061ace0c88c8800751c383b0e80
Baptiste Bauvin <baptiste.bauvin.1@ulaval.ca>
2018-02-12 19:22:21 -0500

    Added difficulty measure

---
+++ multiview_platform/Tests/Test_MultiviewClassifiers/Test_DifficultyMeasure/test_DifficultyMeasureModule.py
@@ -0,0 +1,30 @@
+import unittest
+import numpy as np
+
+from ....MonoMultiViewClassifiers.MultiviewClassifiers.DifficultyFusion import DifficultyFusionModule
+
+class Test_difficulty(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.classifiersDecisions = np.array([
+            [np.random.random_integers(0, 1, (2, 5)), [[0, 0, 1, 0, 1], [0, 1, 0, 1, 0]],
+             np.random.random_integers(0, 1, (2, 5)), np.random.random_integers(0, 1, (2, 5)),
+             np.random.random_integers(0, 1, (2, 5))],
+            [np.random.random_integers(0, 1, (2, 5)), np.random.random_integers(0, 1, (2, 5)),
+             np.random.random_integers(0, 1, (2, 5)), [[0, 0, 1, 1, 0], [0, 1, 0, 1, 0]],
+             np.random.random_integers(0, 1, (2, 5))],
+            [np.random.random_integers(0, 1, (2, 5)), np.random.random_integers(0, 1, (2, 5)),
+             np.random.random_integers(0, 1, (2, 5)), np.random.random_integers(0, 1, (2, 5)),
+             [[0, 1, 1, 1, 1], [0, 1, 0, 1, 0]]],
+        ])
+        cls.combination = [1, 3, 4]
+        cls.foldsGroudTruth = np.array([[1, 1, 0, 0, 1], [0, 1, 0, 1, 0]])
+        cls.foldsLen = ""
+
+    def test_simple(cls):
+        difficulty_measure = DifficultyFusionModule.difficulty(cls.classifiersDecisions,
+                                                               cls.combination,
+                                                               cls.foldsGroudTruth,
+                                                               cls.foldsLen)
+        cls.assertAlmostEqual(difficulty_measure, 0.11111111111)
