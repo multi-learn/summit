1388f13488ce6fb879431d3e7ae837465df3d8ba
Baptiste Bauvin <baptiste.bauvin@lis-lab.fr>
2020-04-07 08:16:28 -0400

    conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -73,7 +73,7 @@ source_suffix = ['.rst', '.md']
 master_doc = 'index'
 
 # General information about the project.
-project = u'MultiviewPlatform'
+project = u'SuMMIT'
 copyright = u'2019, Baptiste BAUVIN'
 author = u'Baptiste BAUVIN'
 
@@ -126,7 +126,7 @@ html_static_path = ["_static/"]
 # -- Options for HTMLHelp output ------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'MultiviewPlatformdoc'
+htmlhelp_basename = 'SuMMITdoc'
 
 # -- Options for LaTeX output ---------------------------------------------
 
@@ -152,7 +152,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'MultiviewPlatform.tex', u'MultiviewPlatform Documentation',
+    (master_doc, 'SuMMIT.tex', u'SuMMIT Documentation',
      u'Baptiste BAUVIN', 'manual'),
 ]
 
@@ -161,7 +161,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'multiviewplatform', u'MultiviewPlatform Documentation',
+    (master_doc, 'summit', u'SuMMIT Documentation',
      [author], 1)
 ]
 
@@ -171,8 +171,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'MultiviewPlatform', u'MultiviewPlatform Documentation',
-     author, 'MultiviewPlatform', 'One line description of project.',
+    (master_doc, 'SuMMIT', u'SuMMIT Documentation',
+     author, 'SuMMIT', 'One line description of project.',
      'Miscellaneous'),
 ]
 
