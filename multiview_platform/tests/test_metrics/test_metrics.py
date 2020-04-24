113d5d88962907a20b3bd9b387a0117fea34fffa
Baptiste Bauvin <baptiste.bauvin@lis-lab.fr>
2020-04-24 17:32:21 -0400

    Frgot to add files

---
+++ multiview_platform/tests/test_metrics/test_metrics.py
@@ -0,0 +1,29 @@
+import unittest
+import multiview_platform.mono_multi_view_classifiers.metrics as metrics
+import pkgutil
+import os
+from sklearn.metrics._scorer import _BaseScorer
+
+# Tester que chaque metrique a bien les bonnes fonctions qui renvoient bien les bons types d'outputs avec les bons types d'inputs
+# Faire de meme pour les differents classifeurs monovues et les differents classifeurs multivues
+
+
+class Test_metric(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.test="a"
+
+
+    def test_simple(self):
+        pkgpath = os.path.dirname(metrics.__file__)
+        for _, metric, _ in pkgutil.iter_modules([pkgpath]):
+            module = getattr(metrics, metric)
+            self.assertTrue(hasattr(module, "score"))
+            self.assertTrue(isinstance(module.score([1,0],[1,0]), float))
+            self.assertTrue(hasattr(module, "get_scorer"))
+            self.assertTrue(isinstance(module.get_scorer(), _BaseScorer))
+            self.assertTrue(hasattr(module, "get_config"))
+            self.assertTrue(isinstance(module.get_config(), str))
+
+
