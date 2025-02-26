2a325003854da30622c054c07c51eba0436231ed
Dominique <dominique.benielli@univ-amu.fr>
2025-02-26 16:28:10 +0100

    Update early_fusion_adaboost.py

--- summit/multiview_platform/multiview_classifiers/early_fusion_adaboost.py
+++ summit/multiview_platform/multiview_classifiers/early_fusion_adaboost.py
@@ -10,14 +10,14 @@ classifier_class_name = "EarlyFusionAdaboost"
 class EarlyFusionAdaboost(BaseEarlyFusion):
 
     def __init__(self, random_state=None, n_estimators=50,
-                 base_estimator=None, base_estimator_config=None, **kwargs):
+                 estimator=None, base_estimator_config=None, **kwargs):
         BaseEarlyFusion.__init__(self, random_state=random_state,
                                  monoview_classifier="adaboost",
                                  n_estimators= n_estimators,
-                                 base_estimator=base_estimator,
+                                 estimator=estimator,
                                  base_estimator_config=base_estimator_config, **kwargs)
-        self.param_names = ["n_estimators", "base_estimator"]
-        self.classed_params = ["base_estimator"]
+        self.param_names = ["n_estimators", "estimator"]
+        self.classed_params = ["estimator"]
         self.distribs = [CustomRandint(low=1, high=500),
                          base_boosting_estimators]
-        self.weird_strings = {"base_estimator": "class_name"}
+        self.weird_strings = {"estimator": "class_name"}
